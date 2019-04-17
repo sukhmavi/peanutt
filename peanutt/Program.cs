c53aa2603bead6e713d80de832969c840eaa3873
visualstudio <visualstudio@122-PC36>
2019-04-17 09:39:31 -0400

    p[eanutt

--- peanutt/Program.cs
+++ peanutt/Program.cs
@@ -4,12 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace peanutt
+namespace peanut
 {
-    class Program
+    class sukh
     {
+        static int i = 0;
         static void Main(string[] args)
         {
+            MethodA();
+            Console.ReadLine();
+
+        }
+
+        public static void MethodA()
+        {
+            while (Peanut())
+            {
+
+                Console.WriteLine("i is {0}", i);
+
+                if (i > 10)
+                {
+                    return;
+                }
+            }
+        }
+        public static bool Peanut()
+        {
+            i++;
+            return true;
         }
     }
 }
